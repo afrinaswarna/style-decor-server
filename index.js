3ba6928c24253051eeb18800f94686b1ace1f30d
swarna18021999 <afrinaswarna1999@gmail.com>
2025-12-11 15:11:57 +0600

    completed style decor server setup

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const app = express()
+const cors  = require('cors')
+require('dotenv').config()
+
+const port = process.env.PORT || 3000
+
+
+app.use(cors())
+app.use(express.json())
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.52cv5mu.mongodb.net/?appName=Cluster0`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+   
+    await client.connect();
+   
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+  res.send('style decor project is running')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
